a93aadf6c44ac7400503f07eb37a823e4b13b761
20hnu <bishnudattbadu77@gmail.com>
2025-04-09 17:31:37 +0545

    Database Cofiguration and ERD added

---
+++ db/db_config.py
@@ -0,0 +1,12 @@
+import pymysql
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Loads variables from .env
+host, user, password, db = os.getenv('host'), os.getenv('user'), os.getenv('password'), os.getenv('db')
+print(host,user,password,db)
+def get_connection():
+    return pymysql.connect(host=host, user=user, password= password, db=db, port=3306,cursorclass=pymysql.cursors.DictCursor)
+
+if __name__ == '__main__':
+    print(get_connection())
